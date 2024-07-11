317b7a94774453bb7fbadfc1a6184c68c2f5a14f
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-07-11 20:16:00 +0530

    cloudinary upload

--- config/CloudinarySiteServer.js
+++ config/CloudinarySiteServer.js
@@ -1,9 +1,17 @@
 // Require the Cloudinary library
-const cloudinary = require('cloudinary').v2
-require('dotenv').config();
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 
+const CloudinarySiteServer = () => {
+  try {
+    cloudinary.config({
+      CLOUD_NAME: process.env.CLOUD_NAME,
+      API_KEY: process.env.API_KEY,
+      API_SECRECT_KEY: process.env.API_SECRECT_KEY,
+    });
+  } catch (er) {
+    console.log("connection not working in cloud part");
+  }
+};
 
-
-
-
-
+module.exports = CloudinarySiteServer;
